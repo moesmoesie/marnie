e7f9f78a1b39fe63aaf81fb8efd902352116b29e
Mustafa Darwesh <mndarwesh@protonmail.com>
2021-08-16 10:56:04 +0200

    Refactor

---
+++ lib/pages/dream/dream_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:marnie/models/dream_model.dart';
+import 'package:provider/provider.dart';
+
+import 'dream_text_editor.dart';
+
+class DreamBody extends StatefulWidget {
+  const DreamBody({Key? key}) : super(key: key);
+
+  @override
+  _DreamBodyState createState() => _DreamBodyState();
+}
+
+class _DreamBodyState extends State<DreamBody> {
+  @override
+  Widget build(BuildContext context) {
+    final EditableDream dream = Provider.of<EditableDream>(context);
+    return Center(
+      child: DreamTextEditor(dream.title),
+    );
+  }
+}
